43672fecc362d4ac5e523c408e91938655795e5e
Thibaut Tauveron <thibaut.tauveron@gmail.com>
2021-12-12 23:04:02 +0100

    add test docker creation

--- main.ts
+++ main.ts
@@ -1,11 +1,30 @@
 import { Construct } from "constructs";
 import { App, TerraformStack } from "cdktf";
+import { Container, Image, DockerProvider } from './.gen/providers/docker'
 
 class MyStack extends TerraformStack {
   constructor(scope: Construct, name: string) {
     super(scope, name);
 
-    // define resources here
+    new DockerProvider(this, 'default', {})
+
+    const dockerImage = new Image(this, 'nginxImage', {
+      name: 'nginx:latest',
+      keepLocally: false,
+    })
+
+    new Container(this, 'nginxContainer', {
+      image: dockerImage.latest,
+      name: 'tutorial',
+      ports: [
+        {
+          internal: 80,
+          external: 8000,
+        },
+      ],
+    })
+
+
   }
 }
 
